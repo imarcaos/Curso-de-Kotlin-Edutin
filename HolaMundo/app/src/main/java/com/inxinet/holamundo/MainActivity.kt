942e1c29be330d0fea0a97815b9ec5a4cf5c981b
imarcaos <marcaos.melo@gmail.com>
2022-12-22 11:39:57 +0000

    Hola Mundo - Conexion entre Kotlin y XML

--- HolaMundo/app/src/main/java/com/inxinet/holamundo/MainActivity.kt
+++ HolaMundo/app/src/main/java/com/inxinet/holamundo/MainActivity.kt
@@ -2,10 +2,15 @@ package com.inxinet.holamundo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.texto_hola)
+
+        textView.text = "Hola Kotlin"
     }
 }
